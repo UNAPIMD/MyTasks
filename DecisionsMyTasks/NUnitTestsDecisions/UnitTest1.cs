201d059bbb03d71e94dfd9a1b9a1ed4cac234e61
UNAPIMD <ExpIgn@gmail.com>
2024-07-20 11:35:11 +0300

    Добавил пару тестов для Transpose()

--- DecisionsMyTasks/NUnitTestsDecisions/UnitTest1.cs
+++ DecisionsMyTasks/NUnitTestsDecisions/UnitTest1.cs
@@ -645,6 +645,16 @@ namespace NUnitTestsDecisions
             //Размер 0x0
             Assert.That(() => Transpose(new int[,] { }), Throws.TypeOf<ArgumentOutOfRangeException>());
 
+            Matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            //Матрица не квадратная
+            Assert.That(() => Transpose(Matrix), Throws.TypeOf<ArgumentOutOfRangeException>());
+
+            Matrix = new int[,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+
+            //Матрица не квадратная
+            Assert.That(() => Transpose(Matrix), Throws.TypeOf<ArgumentOutOfRangeException>());
+
             Matrix = null;
 
             Assert.That(() => Transpose(Matrix), Throws.TypeOf<ArgumentNullException>());
